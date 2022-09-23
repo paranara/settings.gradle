613423053f52d71c74dc9c90bbb546406ac8d2bc
paranora <yangqun@centaline.com.cn>
2022-09-23 17:43:13 +0800

    by paranora 2022 09 23 18:43

--- spring-mapstruct-starter-test/src/main/java/org/paranora/mapstruct/starter/test/entity/dto/StaffRequestDTO.java
+++ spring-mapstruct-starter-test/src/main/java/org/paranora/mapstruct/starter/test/entity/dto/StaffRequestDTO.java
@@ -2,15 +2,20 @@ package org.paranora.mapstruct.starter.test.entity.dto;
 
 
 import lombok.*;
-import lombok.experimental.SuperBuilder;
+import org.paranora.mapstruct.annotations.PMapper;
+import org.paranora.mapstruct.annotations.PMapping;
+import org.paranora.mapstruct.starter.test.entity.Staff;
 
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@PMapper(target = Staff.class)
 public class StaffRequestDTO {
     private String staffName;
     private Integer age;
+
+    @PMapping(target = "sex")
     private Integer staffSex;
     private String birth;
     private CompanyRequestDTO staffCompany;
